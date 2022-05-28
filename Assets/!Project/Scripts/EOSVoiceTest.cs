5fafb82846fc3d75c8c0a126a164dd9ee51bb232
Maximilian Maroe <drakfyre@gmail.com>
2022-05-27 22:24:36 -0700

    Just some minor updates and a stub

---
+++ Assets/!Project/Scripts/EOSVoiceTest.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Epic.OnlineServices;
+
+public class EOSVoiceTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    { 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
--- Assets/!Project/Scripts/Editor/GUIDAssetPostprocessor.cs
+++ Assets/!Project/Scripts/Editor/GUIDAssetPostprocessor.cs
@@ -7,19 +7,8 @@ class GUIDAssetPostprocessor : AssetPostprocessor
     // This happens anytime the project changes... probably can find a better place for this.
     static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths, bool didDomainReload)
     {
-        string path = Path.Combine(Application.dataPath,"Plugins/Android/libs/eos-sdk.aar");
-        Debug.Log("Doing something");
-        // Not GUID related (so I should rename this class) but I also need to make sure that Plugins has been moved
-        if(!File.Exists(path))
-        {
-            Debug.Log("ACTUALLY doing something");
-            string sourcePath = Path.Combine(Application.dataPath, "EpicOnlineTransport/Plugins/");
-            string destinationPath = Path.Combine(Application.dataPath, "Plugins");
-            FolderCopyUtility.CopyDirectory(sourcePath,destinationPath,true);
-        }
-
         // I chose StreamingAssets because it will guarentee your builds get the file
-        path = Path.Combine(Application.streamingAssetsPath, "MultiplayerGUID.guid");
+        string path = Path.Combine(Application.streamingAssetsPath, "MultiplayerGUID.guid");
 
         // Early exit if we already have a GUID file
         if(File.Exists(path))
